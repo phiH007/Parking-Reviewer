4f67466c21e0ee4d377eff50f0818a8545a7b8b0
Phi <phoang1.618@gmail.com>
2026-03-24 22:40:52 -0400

    add login and update main to include login. No funcationality added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,18 @@ import React from 'react';
 import Header from './components/header';
 import Sidebar from './components/sidebar';
 import Main from './components/main';
+import Login from './components/login'
 import './App.css';
 
+// need to figure out routing and connect login page to mongodb. Opens by default to login page
 function App() {
   return (
     <div className="app-container">
       <Header />
       <div className="content-layout">
-        <Sidebar />
-        <Main />
+        <Login />
+        {/* <Sidebar />
+        <Main /> */}
       </div>
     </div>
   );
---
+++ frontend/src/components/login.jsx
@@ -0,0 +1,22 @@
+const Login = () => {
+    return (
+        <div className="login-container">
+            <div className="login-box">
+            <h2>Login</h2>
+            <form>
+                <div>
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" placeholder="Enter your email" />
+                </div>
+                <div>
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" placeholder="Enter your password" />
+                </div>
+                <button type="submit">Login</button>
+             </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login
